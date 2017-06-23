e8735f7970415313e842b65ae18ae11fc42a3d20
alexsevv <aaa123qqq@mail.ru>
2017-06-23 18:52:55 +0300

    sugar

--- kanobu.rb
+++ kanobu.rb
@@ -8,7 +8,7 @@ my_choice = gets.chomp.to_i
 computer_choices = rand(4)
 results = ["Ваша ПОБЕДА!", "Ничья", "Ваш проигрыш"]
 
-def find_winner(you, computer, results_game)
+def find_winner(you, computer)
   #все победные комбинации для юзера
   if (you == 0 && computer == 1) ||
     (you == 1 && computer == 2) ||
@@ -16,17 +16,17 @@ def find_winner(you, computer, results_game)
     (you == 0 && computer == 3) ||
     (you == 3 && computer == 2) ||
     (you == 3 && computer == 1)
-    return results_game[0]
+    0
   #если ничья
   elsif you == computer
-    return results_game[1]
+    1
   #все остальные варианты - проигрыш юзера
   else
-    return results_game[2]
+    2
   end
 end
 
 puts 'Вы выбрали: ' + variants[my_choice]
 puts 'Компьютер выбрал: ' + variants[computer_choices]
 puts
-puts 'Результат игры: ' + find_winner(my_choice, computer_choices, results)
+puts 'Результат игры: ' + results[find_winner(my_choice, computer_choices)]
